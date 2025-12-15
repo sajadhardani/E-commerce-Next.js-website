77b5d1f6cea21dd1660123072babbc69c5643cb6
sajad <sajad.hardani188@gmail.com>
2025-12-15 14:07:14 +0330

    add: Get Data from prisma db

--- src/modules/products/components/ProductList.tsx
+++ src/modules/products/components/ProductList.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { DATA } from '../mock/products';
 import ProductItem from './ProductItem';
 import {PrismaType} from '@/lib/prisma'
+import { ProductsWithImages } from '@/types';
 // prismaType 
-function ProductList() {
+function ProductList(props:{products:ProductsWithImages[]}) {
+  const {products} = props
   return (
     <div className="flex flex-wrap justify-between items-center w-full my-10">
-      {DATA.map((item) => {
+      {products.map((item) => {
         return <ProductItem key={item.name} product={item} />;
       })}
     </div>
